4c7aeb1b534c710bcdaecb61b9c221d14ed6733b
johnwayodi <wayodijohn@gmail.com>
2021-09-28 11:48:29 +0300

    chore: update navbar, add search & sort to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,73 @@
 import React, { FC } from 'react';
-import { Menu, Row, Typography } from 'antd';
-import { Link } from 'dva/router';
-import { ProfileOutlined } from '@ant-design/icons';
+import { Row, Typography } from 'antd';
+import { connect } from 'dva';
+import { Link, routerRedux } from 'dva/router';
+import {} from '@ant-design/icons';
+import { GlobalState, CountrySummary } from '../models/interfaces';
+import { Dispatch } from '../models/dispatch';
+
+import { SearchBar } from './index';
 
 import styles from './NavBar.module.scss';
 
-const { SubMenu } = Menu;
 const { Title } = Typography;
 
-const NavBar: FC = () => {
+interface Props {
+  loading: boolean;
+  globalSummary: GlobalState;
+  countries: CountrySummary[];
+  countrySearch: CountrySummary[];
+  lastUpdated: string;
+  sortVisible: boolean;
+  getSummary: () => void;
+  viewCountryDetails: (code: string) => void;
+  filterCountries: (name: string) => void;
+  sortCountries: (key: string) => void;
+}
+
+const NavBar: FC<Props> = (props) => {
   return (
     <Row className={styles.navBar}>
       <Row className={styles.navTitle}>
         <Link to="/summary">
-          <Title level={4}>Coronavirus (Covid-19) Statistics</Title>
+          <Title level={4}>Coronavirus Statistics (COVID-19)</Title>
         </Link>
       </Row>
-      <Row className={styles.navMenu}>
-        <Menu selectedKeys={['mail']} mode="horizontal">
-          <Menu.Item key="app" icon={<ProfileOutlined />}>
-            <Link to="#">About</Link>
-          </Menu.Item>
-        </Menu>
-      </Row>
+      <SearchBar
+        countries={props.countrySearch}
+        sortVisible={props.sortVisible}
+        filterCountries={props.filterCountries}
+        viewSelected={props.viewCountryDetails}
+        sortCountries={props.sortCountries}
+      />
     </Row>
   );
 };
 
-export default NavBar;
+const mapStateToProps = ({ global, country, loading }: any) => ({
+  globalSummary: global.summary,
+  countries: global.countries,
+  countrySearch: global.searchResults,
+  lastUpdated: global.date,
+  sortVisible: country.sortVisible,
+  loading: loading.effects['global/fetchSummary'] || loading.effects['global/sortCountries'],
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  getSummary: () => {
+    dispatch({ type: 'global/fetchSummary' });
+  },
+  viewCountryDetails: (slug: string) => {
+    dispatch(routerRedux.push(`/summary/${slug}`));
+  },
+
+  filterCountries: (name: string) => {
+    dispatch({ type: 'global/searchCountry', payload: { name } });
+  },
+
+  sortCountries: (key: string) => {
+    dispatch({ type: 'global/sortCountries', payload: { key } });
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,6 +6,7 @@ import styles from './SearchBar.module.scss';
 
 interface Props {
   countries: CountrySummary[];
+  sortVisible: boolean;
   filterCountries: (name: string) => void;
   viewSelected: (slug: string) => void;
   sortCountries: (key: string) => void;
@@ -16,14 +17,12 @@ const { Text } = Typography;
 
 const sortOptions = [
   { value: 'newConfirmed', text: 'New Cases' },
-  { value: 'newRecovered', text: 'New Recovered' },
   { value: 'newDeaths', text: 'New Deaths' },
   { value: 'totalConfirmed', text: 'Total Cases' },
-  { value: 'totalRecovered', text: 'Total Recovered' },
   { value: 'totalDeaths', text: 'Total Deaths' },
 ];
 
-const SearchBar: FC<Props> = ({ countries, filterCountries, sortCountries, viewSelected }) => {
+const SearchBar: FC<Props> = ({ countries, sortVisible, filterCountries, sortCountries, viewSelected }) => {
   const [result, setResult] = useState<CountrySummary[]>([]);
   const [searchValue, setSearchValue] = useState<string>('');
   const [sortKey, setSortKey] = useState<string>('');
@@ -51,7 +50,6 @@ const SearchBar: FC<Props> = ({ countries, filterCountries, sortCountries, viewS
   const handleSelected = (value: string) => {
     const country = countries.find((element) => element.country === value);
     if (country) {
-      console.log('COUNTRY', country);
       viewSelected(country.slug);
     }
   };
@@ -67,21 +65,32 @@ const SearchBar: FC<Props> = ({ countries, filterCountries, sortCountries, viewS
           {result &&
             result.map((result) => (
               <Option key={result.country} value={result.country}>
-                {result.country}
+                <div className={styles.nameSection}>
+                  <div className={styles.nameText}>{result.country}</div>
+                  <img
+                    src={'https://www.countryflags.io/' + result.countryCode + '/flat/48.png'}
+                    loading="eager"
+                    width="28"
+                    height="28"
+                    alt={result.countryCode}
+                  />
+                </div>
               </Option>
             ))}
         </AutoComplete>
       </Row>
-      <Row className={styles.sortSection}>
-        <Text className={styles.sortText}>Sort By: </Text>
-        <Select className={styles.select} defaultValue={sortOptions[5].value} onChange={handleSort}>
-          {sortOptions.map((sortOption) => (
-            <Option key={sortOption.value} value={sortOption.value}>
-              {sortOption.text}
-            </Option>
-          ))}
-        </Select>
-      </Row>
+      {sortVisible && (
+        <Row className={styles.sortSection}>
+          <Text className={styles.sortText}>Sort By: </Text>
+          <Select className={styles.select} defaultValue={sortOptions[3].value} onChange={handleSort}>
+            {sortOptions.map((sortOption) => (
+              <Option key={sortOption.value} value={sortOption.value}>
+                {sortOption.text}
+              </Option>
+            ))}
+          </Select>
+        </Row>
+      )}
     </Row>
   );
 };
